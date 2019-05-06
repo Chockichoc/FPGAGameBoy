7aebd5511f3d2dbf358bd50dafd849e4bc6a3eec
Chockichoc <alexandre.yos@gmail.com>
2019-05-06 21:27:42 +0200

    VGA driver changes to improve the video sync

--- testVGA.v
+++ testVGA.v
@@ -24,47 +24,52 @@ wire [159:0] bufferOutput1;
 
 reg wr_buffer = 1'b0;
 
+parameter hori_line  = 800;                           
+parameter hori_back  = 144;
+parameter hori_front = 16;
+parameter vert_line  = 525;
+parameter vert_back  = 34;
+parameter vert_front = 11;
+parameter H_sync_cycle = 96;
+parameter V_sync_cycle = 2;
+parameter H_BLANK = hori_front+H_sync_cycle ;
+
 videoRam ppuBuffer0(pixelClk,
 	LineBuffer0,
-	VCount,
+	YValid,
 	oldLY,
 	wr_buffer,
 	bufferOutput0);
 
 videoRam ppuBuffer1(pixelClk,
 	LineBuffer1,
-	VCount,
+	YValid,
 	oldLY,
 	wr_buffer,
 	bufferOutput1);
 
 
-always @(posedge pixelClk) begin
+always @(negedge pixelClk) begin
 	
-	if(HCountMax) begin
-		HCount <= 10'd0;
-
-	end
-	else begin
+	if(HCount == hori_line - 1'b1)
+		begin
+			HCount <= 10'd0;
+			
+			if(VCount == vert_line - 1'b1)
+				VCount <= 10'd0;
+			else
+				VCount <= VCount + 1'b1;
+		
+		end
+	else
 		HCount <= HCount + 1'b1;
 
-
-	end
-
 end
 
-always @(posedge HCountMax) begin
-	
-	if(VCountMax)
-		VCount <= 10'd0;
-	else 
-		VCount <= VCount + 1'b1;
-	
-		
-end
 
-assign HSync = (HCount >= 10'd656 && HCount <= 10'd751) ? 1'b0 : 1'b1;
-assign VSync = (VCount >= 10'd492 && VCount <= 10'd493) ? 1'b0 : 1'b1;
+
+assign HSync = (HCount < H_sync_cycle) ? 1'b0 : 1'b1;
+assign VSync = (VCount < V_sync_cycle) ? 1'b0 : 1'b1;
 
 reg [1:0] saveRoutineCounter = 2'b00;
 
@@ -90,22 +95,28 @@ always @(posedge pixelClk) begin
 
 end
 
-assign R = (VCount < 10'd144 && HCount < 10'd160) ? 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b00 ? 4'b1101 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b01 ? 4'b1000 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b10 ? 4'b0011 : 
+wire [9:0] XValid;
+wire [9:0] YValid;
+
+assign XValid = HCount - hori_back;
+assign YValid = VCount - vert_back;
+
+assign R = (YValid >= 10'd0 && YValid < 10'd145 && XValid >= 10'd0 && XValid < 10'd160) ? 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b00 ? 4'b1101 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b01 ? 4'b1000 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b10 ? 4'b0011 : 
    4'b0001))) : 4'b0000;
    
-assign G = (VCount < 10'd144 && HCount < 10'd160) ? 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b00 ? 4'b1111 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b01 ? 4'b1011 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b10 ? 4'b0110 : 
+assign G = (YValid >= 10'd0 && YValid < 10'd145 && XValid >= 10'd0 && XValid < 10'd160) ? 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b00 ? 4'b1111 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b01 ? 4'b1011 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b10 ? 4'b0110 : 
    4'b0001))) : 4'b0000;
    
-assign B = (VCount < 10'd144 && HCount < 10'd160) ? 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b00 ? 4'b1100 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b01 ? 4'b0111 : 
-   ({bufferOutput1[HCount], bufferOutput0[HCount]} == 2'b10 ? 4'b0101 : 
+assign B = (YValid >= 10'd0 && YValid < 10'd145 && XValid >= 10'd0 && XValid < 10'd160) ? 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b00 ? 4'b1100 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b01 ? 4'b0111 : 
+   ({bufferOutput1[XValid], bufferOutput0[XValid]} == 2'b10 ? 4'b0101 : 
    4'b0010))) : 4'b0000;
 
 
